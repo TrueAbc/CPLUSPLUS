39b527eada637c2ff5072ca9f3592de1312d97fa
trueabc <547551933@qq.com>
2021-09-05 11:32:42 +0800

    开放头文件引用

---
+++ includes/lars_reactor.h
@@ -0,0 +1,28 @@
+//
+// Created by TrueAbc on 2021/9/5.
+//
+
+#ifndef LARS_LARS_REACTOR_H
+#define LARS_LARS_REACTOR_H
+
+// 添加所有必须的头文件
+#include "io_buf.h"
+#include "buf_pool.h"
+#include "reactor_buf.h"
+
+#include "net_connection.h"
+#include "tcp_conn.h"
+#include "tcp_server.h"
+#include "tcp_client.h"
+#include "udp_server.h"
+#include "udp_client.h"
+
+#include "message.h"
+#include "task_msg.h"
+#include "event_loop.h"
+#include "thread_pool.h"
+#include "thread_queue.h"
+
+#include "config_file.h"
+
+#endif //LARS_LARS_REACTOR_H
